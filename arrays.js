28fa051596373dce9c6a48f06801b45579418ff6
fantastic-page-5704 <chandvakil@gmail.com>
2019-01-08 20:22:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,9 +12,7 @@ var chocolateBars = [
   ]
   
   function destructivelyAddElementToBeginningOfArray(array, element) {
-     array.unshift(element)
-     return array
-  }
+  
   
   function addElementToBeginningOfArray(array, element) {
     return [element, ...array]
